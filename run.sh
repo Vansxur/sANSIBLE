eb2c0b3fb29d726655865dfcb0ece3b6a2c98485
Vansxur <65874354+Vansxur@users.noreply.github.com>
2023-03-31 14:50:32 +0200

    [ Feat ] First commit

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+Audit_Name=$1
+
+# Check if we passed name in arg, if not, ask in a prompt the name for the audit
+if [[ -z $Audit_Name ]]; then
+  echo -n "Enter audit name ( it will erase duplicates ): "
+  read -r Audit_Name
+fi
+
+# Run Ansible with config files
+ansible-playbook -i inventory.yaml playbook.yaml --timeout 60 --extra-vars "audit_folder=$Audit_Name"
+
+# Start craking password session
+./modules/password/cracking_password.sh $Audit_Name
